feebfeb899d078b32a1c9e371886fd56b19ae56f
UfranVF <ufranpatel8291@gmail.com>
2024-02-08 19:20:58 +0530

    random changes

--- App.tsx
+++ App.tsx
@@ -1,10 +1,9 @@
-import {Dimensions, SafeAreaView, ScrollView, StyleSheet, Text, View} from 'react-native';
+import {Dimensions, Keyboard, SafeAreaView, ScrollView, StyleSheet, Text, TextInput, TouchableOpacity, TouchableWithoutFeedback, View} from 'react-native';
 import React, { useState } from 'react';
-
-
 const windowWidth = Dimensions.get('window').width;
 const windowHeight = Dimensions.get('window').height;
 export default function App(): JSX.Element {
+   const [taskValue, setTaskValue] = useState("");
    const [tasks,setTasks]=useState([
     {
       title:'Complete playlist till 13th vid',
@@ -19,8 +18,18 @@ export default function App(): JSX.Element {
   // newTasks.unshift({title:'some tasks',isCompleted:false}); 
   // setTasks(newTasks);
   return (
+    <TouchableWithoutFeedback style={{flex:1}} onPress={()=>{Keyboard.dismiss()}}>
     <SafeAreaView style={styles.container}>
+      
       <Text style={styles.heroTxt}>CamelTodo</Text>
+      <View style={styles.inputContainer}>
+        <Text style={styles.subHeading}>Add a new Task</Text>
+        <TextInput style={styles.inputStyles} value={taskValue}  placeholder='Task Title' placeholderTextColor={'#333'} onChange={(e)=>setTaskValue(e.nativeEvent.text)} />
+        <TouchableOpacity style={styles.button}>
+          
+          <Text>Add Task</Text>
+        </TouchableOpacity>
+      </View>
       <ScrollView style={styles.scroller} horizontal={true} >
 
       <View style={styles.pageContainer}>
@@ -32,6 +41,7 @@ export default function App(): JSX.Element {
       
       </ScrollView>
     </SafeAreaView>
+    </TouchableWithoutFeedback>
   );
 }
 
@@ -45,6 +55,28 @@ const styles = StyleSheet.create({
     fontSize:25,
     color:'#fff',
     fontWeight:'bold',
+    marginVertical:10
+  },
+  inputContainer:{
+    backgroundColor:'#ffffff',
+    height:150,
+
+    width:windowWidth*0.85,
+    marginVertical:10,
+    elevation:5,
+    padding:5,
+    margin:5
+  },
+  inputStyles:{
+    borderColor:'#333',
+    borderWidth:1,
+  },
+  button:{
+    backgroundColor:'#49C144',
+    paddingVertical:10,
+    paddingHorizontal:20,
+    alignItems:'center',
+    width:200,
     marginVertical:10
   },
   pageContainer: {
