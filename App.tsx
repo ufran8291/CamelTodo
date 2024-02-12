4e3964a27388b069b9e36903196d6330c6cb7833
UfranVF <ufranpatel8291@gmail.com>
2024-02-12 21:11:38 +0530

    added  icons and task addition logis

--- App.tsx
+++ App.tsx
@@ -1,19 +1,38 @@
-import {Dimensions, Keyboard, SafeAreaView, ScrollView, StyleSheet, Text, TextInput, TouchableOpacity, TouchableWithoutFeedback, View} from 'react-native';
+import {Alert, Dimensions, Keyboard, SafeAreaView, ScrollView, StyleSheet, Text, TextInput, TouchableOpacity, TouchableWithoutFeedback, View} from 'react-native';
 import React, { useState } from 'react';
+import Ionicons from 'react-native-vector-icons/Ionicons'
+// Add this line to your `index.js`
+import 'react-native-get-random-values'
+import { v4 as uuidv4 } from 'uuid';
 const windowWidth = Dimensions.get('window').width;
 const windowHeight = Dimensions.get('window').height;
 export default function App(): JSX.Element {
    const [taskValue, setTaskValue] = useState("");
    const [tasks,setTasks]=useState([
-    {
-      title:'Complete playlist till 13th vid',
-      isCompleted : false,
-    },
-    {
-      title:'Complete playlist till 13th vid',
-      isCompleted : true,
-    }
+    {taskId:uuidv4(),title:'',isCompleted:false}
+    // {title:"Take medicines",isCompleted:false},{title:"Complete Physics homework",isCompleted:false}
   ]);
+  
+
+  function addNewTask(){
+    if (taskValue != null && taskValue.toString() != '' && taskValue!= undefined) {
+      setTasks([{taskId:uuidv4() ,title:taskValue.toString(),isCompleted:false},...tasks]);
+      setTaskValue('')
+      Keyboard.dismiss()
+    }else{
+      Alert.alert('Please enter the task title','Please enter the title of the task from the above input bar.')
+      return;
+    }
+    //[{}] array of objects
+    // //  [] array of undefined
+    // console.log(tasks);
+    // console.log('before adding new')
+    // var newTasks = tasks;
+    //   newTasks.unshift({title:'some tasks',isCompleted:false}); 
+      // setTasks([{title:taskValue.toString(),isCompleted:false},...tasks]);
+      // console.log('after adding new')
+      // console.log(tasks);
+  }
   // var newTasks =  tasks
   // newTasks.unshift({title:'some tasks',isCompleted:false}); 
   // setTasks(newTasks);
@@ -25,15 +44,27 @@ export default function App(): JSX.Element {
       <View style={styles.inputContainer}>
         <Text style={styles.subHeading}>Add a new Task</Text>
         <TextInput style={styles.inputStyles} value={taskValue}  placeholder='Task Title' placeholderTextColor={'#333'} onChange={(e)=>setTaskValue(e.nativeEvent.text)} />
-        <TouchableOpacity style={styles.button}>
-          
-          <Text>Add Task</Text>
+        <TouchableOpacity style={styles.button} onPress={addNewTask}>
+          <Ionicons name="add-circle" size={35} color={'#fff'}/>
+          <Text style={styles.buttonTxt}>Add Task</Text>
         </TouchableOpacity>
       </View>
       <ScrollView style={styles.scroller} horizontal={true} >
 
       <View style={styles.pageContainer}>
-        <Text style={styles.subHeading}>Remaining Tasks</Text>
+        <Text style={styles.subHeading}>Remaining Task</Text>
+        {tasks.map((task) => {
+          if (task.title !='' ) {
+            return (
+              <View key={task.taskId} >
+                <Text >{task.title}</Text>
+                <Text >{task.taskId}</Text>
+                <Text >{task.isCompleted.toString()}</Text>
+              </View>
+            )
+          }
+       
+      })}
       </View> 
       <View style={styles.pageContainer}>
         <Text style={styles.subHeading}>Completed Tasks</Text>
@@ -59,12 +90,12 @@ const styles = StyleSheet.create({
   },
   inputContainer:{
     backgroundColor:'#ffffff',
-    height:150,
+    height:180,
 
     width:windowWidth*0.85,
     marginVertical:10,
     elevation:5,
-    padding:5,
+    padding:10,
     margin:5
   },
   inputStyles:{
@@ -76,8 +107,16 @@ const styles = StyleSheet.create({
     paddingVertical:10,
     paddingHorizontal:20,
     alignItems:'center',
+    justifyContent:'center',
     width:200,
+    flexDirection:'row',
     marginVertical:10
+  },
+  buttonTxt:{
+    color:'#fff',
+    fontWeight:'bold',
+    marginLeft:10,
+    fontSize:16
   },
   pageContainer: {
     backgroundColor:'#ffffff',
